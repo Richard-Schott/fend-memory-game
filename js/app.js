4e526333557661981201768e3536c739c7de9815
Richard-Schott <36338549+Richard-Schott@users.noreply.github.com>
2018-07-14 16:33:55 -0500

    jQuery
    -added jQuery link
    -started functions and toggle for card

--- js/app.js
+++ js/app.js
@@ -25,6 +25,50 @@ function shuffle(array) {
     return array;
 }
 
+//event listener for a card
+/*
+FUNNY but it causes individual pieces to fade away
+$(document).ready(function() {
+	$('.card').on('click', function(event) {
+		$(this).toggle('i');
+	});
+});
+*/
+
+/*
+$(document).ready(function() {
+	$('.card').on('click', function(event) {
+		$(this).toggle('.fa');
+	});
+});
+*/
+
+function toggleCard() {
+	let side = addEventListener.toggle('.card');
+	if (side.display === "none") {
+		side.display($('.fa'));
+	} else {
+		side.display ($('.card'));
+	}
+	return side;
+}
+
+/*
+function toggleCard() {
+	let side = addEventListener.toggle('.card');
+	if (side.display === "none") {
+		console.log("TEST TRUE");
+	} else {
+		side.display = console.log("TEST FALSE");
+	}
+}
+*/
+	
+/* not a function; cannot use getElementByID with document?
+document.getElementByID('#card').click(console.log('CARD'));
+$('li').getElementById('card').on('click', );
+/*
+
 
 /*
  * set up the event listener for a card. If a card is clicked:

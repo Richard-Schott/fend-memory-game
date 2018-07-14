c758b80a4406d45a9c29bda911d4634e80f71f3f
Richard-Schott <36338549+Richard-Schott@users.noreply.github.com>
2018-07-14 17:28:18 -0500

    Junk code
    -created reference code document

---
+++ js/commented-junk-js.js
@@ -0,0 +1,35 @@
+/*
+function toggleCard() {
+	let side = addEventListener.toggle('.card');
+	if (side.display === "none") {
+		side.display($('.fa'));
+	} else {
+		side.display ($('.card'));
+	}
+	return side;
+}
+*/
+/*
+function toggleCard() {
+	let side = addEventListener.toggle('.card');
+	if (side.display === "none") {
+		console.log("TEST TRUE");
+	} else {
+		side.display = console.log("TEST FALSE");
+	}
+}
+*/
+	
+/* not a function; cannot use getElementByID with document?
+document.getElementByID('#card').click(console.log('CARD'));
+$('li').getElementById('card').on('click', );
+/*
+
+/*
+FUNNY but it causes individual pieces to fade away
+$(document).ready(function() {
+	$('.card').on('click', function(event) {
+		$(this).toggle('i');
+	});
+});
+*/
